a874639862fb29835321dd8e6412107201a2f7f3
muhammed-codes <dimeji.asynctechs@gmail.com>
2026-02-28 12:47:07 +0100

    feat: add Express server for Car API with CRUD operations and initial setup

---
+++ 02_expressjs/server.js
@@ -0,0 +1,82 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+const router = express.Router();
+
+app.use(express.json());
+
+let cars = [
+  { id: 1, make: "Toyota", model: "Camry", year: 2020, price: 25000 },
+  { id: 2, make: "Honda", model: "Civic", year: 2019, price: 22000 },
+  { id: 3, make: "Ford", model: "Mustang", year: 2021, price: 35000 },
+];
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Car API" });
+});
+
+router.get("/", (req, res) => {
+  res.json(cars);
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const car = cars.find((car) => car.id === id);
+  if (!car) return res.status(404).json({ message: "Car not found" });
+  res.json(car);
+});
+
+router.post("", (req, res) => {
+  const { make, model, year, price } = req.body;
+
+  if (!make || !model || !year || !price) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const newCar = {
+    id: cars.length + 1,
+    make,
+    model,
+    year: Number(year),
+    price: Number(price),
+  };
+  cars.push(newCar);
+  res.status(201).json({ message: "Created a new car" });
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { make, model, year, price } = req.body;
+  const carIndex = cars.findIndex((car) => car.id === Number(id));
+  if (carIndex === -1)
+    return res.status(404).json({ message: "Car not found" });
+
+  if (!make || !model || !year || !price) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  cars[carIndex] = {
+    id: Number(id),
+    make,
+    model,
+    year: Number(year),
+    price: Number(price),
+  };
+  res.json({ message: `Updated car with ID: ${id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const carIndex = cars.findIndex((car) => car.id === Number(id));
+  if (carIndex === -1)
+    return res.status(404).json({ message: "Car not found" });
+  cars.splice(carIndex, 1);
+  res.json({ message: `Deleted car with ID: ${id}` });
+});
+
+app.use("/api/v1/cars", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
